b6400519ed24041208c2f8d24c5622e21fee8112
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-08-18 23:07:46 -0700

    Added pdf output

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fpdf import FPDF
+
 class Bill:
 
     def __init__(self, amount, period):
@@ -19,15 +21,43 @@ class Roommate:
 # creates pdf file that contains data such as names/each bill amount
 class PDFReport:
 
-    def __init__(self, roommate):
-        self.roommate = roommate
+    def __init__(self, filename):
+        self.filename = filename
 
     def generate(self, roommate1, roommate2, bill):
-        pass
+        pdf = FPDF('P', 'pt', 'A4')
+        pdf.add_page()
 
-bill = Bill(amount=130, period='August 2021')
+        pdf = FPDF()
+        pdf.add_page()
+
+        # file params
+        pdf.set_font('Times', 'B', 24)
+        pdf.cell(60, 0, '')
+        pdf.cell(-60, 10, 'Split Utilities Bill', 'C')
+
+        # bill date
+        pdf.set_font('Times', 'B', 16,)
+        pdf.cell(20, 40, txt='Period:',)
+        pdf.cell(-20, 40, txt=bill.period,)
+
+        # payers info/ billing cycle amount
+        pdf.cell(7, 65, payer1.first_name[0]+'.',)
+        pdf.cell(70, 65, payer1.last_name+' '+'payment due:',)
+        pdf.cell(-77, 65, str(payer1.payment(bill, roommate2)),)
+
+        pdf.cell(7, 85, payer2.first_name[0] + '.', )
+        pdf.cell(70, 85, payer2.last_name + ' ' + 'payment due:', )
+        pdf.cell(-1, 85, str(payer2.payment(bill, roommate1)), )
+
+        pdf.output(self.filename)
+
+pay_amount = Bill(amount=130, period='July 2021')
 payer1 = Roommate(first_name='John', last_name='Smith', days_in_place=14)
 payer2 = Roommate(first_name='Sam', last_name='Tarly', days_in_place=25)
 
-print(payer1.first_name, payer1.last_name, 'pays:', payer1.payment(bill=bill, roommate2=payer2))
-print(payer2.first_name, payer2.last_name, 'pays:', payer2.payment(bill=bill, roommate2=payer1))
+print(payer1.first_name, payer1.last_name, 'pays:', payer1.payment(bill=pay_amount, roommate2=payer2))
+print(payer2.first_name, payer2.last_name, 'pays:', payer2.payment(bill=pay_amount, roommate2=payer1))
+
+pdf_report = PDFReport(filename='report.pdf')
+pdf_report.generate(roommate1=payer1, roommate2=payer2, bill=pay_amount)
