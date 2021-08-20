4e136f36fcecbc9f1292c337e7cf75f7b64c2007
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-08-19 21:38:46 -0700

    Changed value to dynamic cli input

--- main.py
+++ main.py
@@ -46,29 +46,43 @@ class PDFReport:
         pdf.cell(-60, 35, 'Split Utilities Bill', 'B')
 
         # bill date
-        pdf.set_font('Times', 'B', 16,)
+        pdf.set_font('Times', 'B', 16)
         pdf.cell(20, 65, 'Period:',)
-        pdf.cell(-20, 65, bill.period,)
-
-        pdf.set_font('Times', '', 16,)
+        pdf.cell(-20, 65, bill.period.capitalize())
+        pdf.set_font('Times', '', 16)
 
         # payers info/ billing cycle amount
-        pdf.cell(7, 85, payer1.first_name[0]+'.')
-        pdf.cell(70, 85, payer1.last_name+' '+'payment due:')
+        pdf.cell(7, 85, payer1.first_name[0].capitalize() + '.', )
+        pdf.cell(70, 85, payer1.last_name.capitalize()+' '+'payment due is:')
         pdf.cell(-77, 85, payer1_bill)
 
-        pdf.cell(7, 100, payer2.first_name[0] + '.', )
-        pdf.cell(70, 100, payer2.last_name + ' '+'payment due:')
+        pdf.cell(7, 100, payer2.first_name[0].capitalize() + '.', )
+        pdf.cell(70, 100, payer2.last_name.capitalize() + ' '+'payment due is:')
         pdf.cell(-1, 100, payer2_bill)
 
         pdf.output(self.filename)
 
-pay_amount = Bill(amount=130, period='July 2021')
-payer1 = Roommate(first_name='John', last_name='Smith', days_in_place=14)
-payer2 = Roommate(first_name='Sam', last_name='Tarly', days_in_place=25)
 
-print(payer1.first_name, payer1.last_name, payer1.payment(bill=pay_amount, roommate2=payer2))
-print(payer2.first_name, payer2.last_name, payer2.payment(bill=pay_amount, roommate2=payer1))
+# hello = input('Hello! I\'ll walk you through the bill splitting process.\nPress "Enter" to move forward ')
+user_firstname = input('Enter your first name, please ')
+user_lastname = input('Enter your lastname, please ')
+billing_period = input('What is the billing date? ')
+total_amount = float(input('Enter the total amount of the bill: '))
+total_days_user = int(input('How many days have you spent at home during the billing period:\n'+billing_period+'?'))
+
+roommate_firstname = input('Enter your roommate first name, please ')
+roommate_lastname = input('Enter your roommate lastname, please ')
+total_days_roommate = int(input(f'How many days {roommate_firstname+""+roommate_lastname} spent at home during the billing period:\n'+billing_period+'?'))
+
+print('The total for', billing_period, 'is:', total_amount)
+
+
+pay_amount = Bill(total_amount, billing_period)
+payer1 = Roommate(user_firstname, user_lastname, total_days_user)
+payer2 = Roommate(roommate_firstname, roommate_lastname, total_days_roommate)
+
+print(payer1.first_name, payer1.last_name, payer1.payment(pay_amount, payer2))
+print(payer2.first_name, payer2.last_name, payer2.payment(pay_amount, payer1))
 
 pdf_report = PDFReport(filename='report.pdf')
 pdf_report.generate(roommate1=payer1, roommate2=payer2, bill=pay_amount)
