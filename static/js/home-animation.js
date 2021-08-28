ada27c7f046838bc9dfeb8f93436bef7d6586667
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-08-27 20:25:21 -0700

    Added home-page animation

---
+++ static/js/home-animation.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let opacity = 0;
+const intervalID = 0;
+
+const show = () => {
+  const body = document.getElementById('body');
+  const btn = document.querySelector('.button');
+
+  opacity = Number(window.getComputedStyle(body).getPropertyValue('opacity'));
+  if (opacity < 1) {
+    opacity = opacity + 0.1;
+    setTimeout(() => {
+      body.style.opacity = opacity;
+    }, 200);
+    setTimeout(() => {
+      btn.style.opacity = opacity;
+      btn.classList.add('bounce');
+    }, 1200);
+  } else {
+    clearInterval(intervalID);
+  }
+};
+
+const fadeIn = () => {
+  setInterval(show, 200);
+};
+
+window.onload = fadeIn;
