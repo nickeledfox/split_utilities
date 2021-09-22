4781dc07cdf8f47d5103cb039d5ffd09db87ad53
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-09-21 20:17:09 -0700

    updated

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template, request
 from flask.views import MethodView
-from wtforms import Form
+from wtforms import Form, StringField
 
 app = Flask(__name__)
 
 class HomePage(MethodView):
 
     def get(self):
-        return render_template('index.html')    
+        return render_template('index.html')
 
 
 class BillFormPage(MethodView):
 
     def get(self):
-        # bill_form = BillForm()
-        return render_template('form.html')
+        bill_form = BillForm()
+        return render_template('form.html', form=bill_form)
 
 class AmountPage(MethodView):
     def post(self):
@@ -24,22 +24,33 @@ class AmountPage(MethodView):
         
 
 class BillForm(Form):
-    pass
+    user_firstname = StringField("First Name",)
+    user_last_name = StringField("Last Name",)
+    user_email = StringField("Email Address",)
+    billing_period = StringField("Billing date",)
+    user_days_spent = StringField\
+        ("Days spent at home",
+         render_kw={"placeholder":
+                        "(during the billing period)"})
+
+    bill_total = StringField\
+        ("Total amount",
+         render_kw={"placeholder":
+                        "Bill total amount"})
+
+    roommate_fn = StringField("First Name",)
+    roommate_ln = StringField("Last Name",)
+    roommate_email = StringField("Email Address")
+    roommate_days_spent = StringField\
+        ("Days roommate spent at home",
+         render_kw={"placeholder":
+                        "(during the billing period)"})
+    
 
 @app.route('/', methods =["GET", "POST"])
-def gfg():
-    if request.method == "POST":
-       user_first_name = request.form.get("user_firstname")
-       user_last_name = request.form.get("user_firstname")
-       user_email = request.form.get("user_email")
-       bill_total = request.form.get("bill_total")
-       user_days_spent = request.form.get("user_days_spent")
-       roommate_fn = request.form.get("roommate_fn")
-       roommate_ln = request.form.get("roommate_ln")
-       roommate_email = request.form.get("roommate_email")
-       roommate_days_spent = request.form.get("roommate_days_spent")
-       return render_template('calculated.html')
-    return render_template("form.html")
+def index():
+    return render_template('index.html')
+
 
 app.add_url_rule('/', view_func=HomePage.as_view('home_page'))
 app.add_url_rule('/form', view_func=BillFormPage.as_view('form'))
