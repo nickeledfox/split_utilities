647afa9a11caa4f9ccc2ad39eb0da8272c879648
mia-7-7 <81501711+mia-7-7@users.noreply.github.com>
2021-09-22 19:30:40 -0700

    Updated for better readability

--- split/input.py
+++ split/input.py
@@ -1,13 +1,22 @@
-user_firstname = input('Enter your first name, please ')
-user_lastname = input('Enter your lastname, please ')
-billing_period = input('What is the billing date? ')
-total_amount = float(input('Enter the total amount of the bill: '))
+user_firstname = input\
+    ('Enter your first name, please ')
+user_lastname = input\
+    ('Enter your lastname, please ')
+billing_period = input\
+    ('What is the billing date? ')
+total_amount = float\
+    (input('Enter the total amount of the bill: '))
 total_days_user = int(
-    input('How many days have you spent at home during the billing period:\n'+billing_period+'?'))
+    input
+    ('How many days have you spent at home during the billing period:'
+     '\n'+billing_period+'?'))
 
-roommate_firstname = input('Enter your roommate first name, please ')
-roommate_lastname = input('Enter your roommate lastname, please ')
+roommate_firstname = input\
+    ('Enter your roommate first name, please ')
+roommate_lastname = input\
+    ('Enter your roommate lastname, please ')
 total_days_roommate = int(
     input(f'How many days '
           f'{roommate_firstname+" "+roommate_lastname}'
-          f' spent at home during the billing period:\n'+billing_period+'?'))
+          f' spent at home during the billing period:'
+          f'\n'+billing_period+'?'))
--- split/main.py
+++ split/main.py
@@ -3,16 +3,18 @@ from fpdf import FPDF
 from input import *
 from params import Bill, Roommate
 
-# create pdf file
+# Create pdf file
 class PDFReport:
 
     def __init__(self, filename):
         self.filename = filename
 
     def generate(self, roommate1, roommate2, bill):
-        # payment amount
-        payer1_bill = str(round(payer1.payment(bill, roommate2), 2))
-        payer2_bill = str(round(payer2.payment(bill, roommate1), 2))
+        # Payment amount
+        payer1_bill = str\
+            (round(payer1.payment(bill, roommate2), 2))
+        payer2_bill = str\
+            (round(payer2.payment(bill, roommate1), 2))
 
         pdf = FPDF('P', 'pt', 'A4')
         pdf.add_page()
@@ -20,26 +22,30 @@ class PDFReport:
         pdf = FPDF()
         pdf.add_page()
 
-        # file params
+        # File params
         pdf.set_font('Times', 'B', 24)
         pdf.cell(60, 0, '')
 
-        # image/logo
+        # Image/logo
         pdf.image('img/house.png', x=95, y=5, w=15, h=15)
 
-        # title
+        # Title
         pdf.set_text_color(105, 105, 105)
         pdf.cell(-60, 35, 'Split Utilities Bill', 'C')
 
-        # bill date and total
+        # Bill date and total
         pdf.set_font('Times', 'B', 16)
         pdf.cell(1, 60, 'Period: '+bill.period.capitalize())
 
         pdf.set_font('Times', '', 16)
-        pdf.cell(5, 75, 'Payers: '+payer1.first_name.capitalize()+" "+payer1.last_name.capitalize()+' and '+payer2.first_name.capitalize()+" "+payer2.last_name.capitalize())
+        pdf.cell(5, 75, 'Payers: '
+                 +payer1.first_name.capitalize()+" "
+                 +payer1.last_name.capitalize()+' and '
+                 +payer2.first_name.capitalize()+" "
+                 +payer2.last_name.capitalize())
         pdf.cell(1, 90, 'Total:'+" "+str(total_amount))
 
-        # payers info/ billing cycle amount
+        # Payers info/ billing cycle amount
         pdf.cell(5, 115, payer1.first_name[0].capitalize() + '.'+' ')
         pdf.cell(60, 115, payer1.last_name.capitalize()+' '+'payment due is: ')
         pdf.cell(-65, 115, payer1_bill)
@@ -52,11 +58,21 @@ class PDFReport:
 
 
 pay_amount = Bill(total_amount, billing_period)
-payer1 = Roommate(user_firstname, user_lastname, total_days_user)
-payer2 = Roommate(roommate_firstname, roommate_lastname, total_days_roommate)
+payer1 = Roommate\
+    (user_firstname,
+     user_lastname,
+     total_days_user)
+payer2 = Roommate\
+    (roommate_firstname,
+     roommate_lastname,
+     total_days_roommate)
 
-print(payer1.first_name, payer1.last_name, payer1.payment(pay_amount, payer2))
-print(payer2.first_name, payer2.last_name, payer2.payment(pay_amount, payer1))
+print(payer1.first_name,
+      payer1.last_name,
+      payer1.payment(pay_amount, payer2))
+print(payer2.first_name,
+      payer2.last_name,
+      payer2.payment(pay_amount, payer1))
 
 pdf_report = PDFReport(filename='report.pdf')
 pdf_report.generate(roommate1=payer1, roommate2=payer2, bill=pay_amount)
